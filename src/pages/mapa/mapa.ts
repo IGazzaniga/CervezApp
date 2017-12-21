39bbeeb903429d24eb161b652277bd6f2a9057ef
IGazzaniga <ignagazzaniga@hotmail.com.ar>
2017-12-21 15:40:29 -0300

    Se añade el mapa

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+
+declare var google;
 
 /**
  * Generated class for the MapaPage page.
@@ -15,12 +18,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapaPage {
   segment = 'mapa';
+  map: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapaPage');
+  }
+
+  ionViewWillEnter(){
+    this.getPosition();
+  }
+
+  getPosition():any{
+    this.geolocation.getCurrentPosition().then(response => {
+      this.loadMap(response);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }
+
+  loadMap(position: Geoposition){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    
+    // create a new map by passing HTMLElement
+    let mapEle: HTMLElement = document.getElementById('map');
+     
+    // create LatLng object
+    let myLatLng = {lat: latitude, lng: longitude};
+  
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+    console.log(mapEle);
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      let marker = new google.maps.Marker({
+        position: myLatLng,
+        map: this.map,
+        title: 'Hello World!'
+      });
+      mapEle.classList.add('show-map');
+    });
   }
 
   public selectSegment(select) {
